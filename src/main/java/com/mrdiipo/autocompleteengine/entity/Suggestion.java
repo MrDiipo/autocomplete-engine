ab30e4f253f24e77388c957644edca61f7f2015f
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-19 22:07:44 +0100

    Suggestion class impl.

---
+++ src/main/java/com/mrdiipo/autocompleteengine/entity/Suggestion.java
@@ -0,0 +1,22 @@
+package com.mrdiipo.autocompleteengine.entity;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode
+// Model class for the suggestion returned by the engine
+public class Suggestion {
+
+    @ToString.Include
+    //  returned suggestion
+    private String suggestion;
+    // the url returned
+    private String target;
+    // defines the type of suggestion
+    private String group;
+    // score of returned suggestion
+    double score;
+}
