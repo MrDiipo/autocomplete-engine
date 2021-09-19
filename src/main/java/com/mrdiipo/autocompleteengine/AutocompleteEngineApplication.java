0af6511f713de1fda1da3cd57de19de9f8bfe712
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-19 21:52:14 +0100

    Initial commit.

---
+++ src/main/java/com/mrdiipo/autocompleteengine/AutocompleteEngineApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.autocompleteengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompleteEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompleteEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrdiipo/autocompleteengine/AutocompleteEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.autocompleteengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
