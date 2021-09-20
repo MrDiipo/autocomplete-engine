e0b36e9e6930e974ebf5ed7cc838478a11fdbcff
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-20 16:28:12 +0100

    Controller updates.

--- src/main/java/com/mrdiipo/autocompleteengine/controller/RestApiController.java
+++ src/main/java/com/mrdiipo/autocompleteengine/controller/RestApiController.java
@@ -3,15 +3,15 @@ package com.mrdiipo.autocompleteengine.controller;
 
 import com.mrdiipo.autocompleteengine.entity.Suggestion;
 import com.mrdiipo.autocompleteengine.service.DataIngestionService;
+import com.mrdiipo.autocompleteengine.service.RapidSuggestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestApiController {
@@ -19,12 +19,28 @@ public class RestApiController {
     @Autowired
     private DataIngestionService dataIngestionService;
 
+    @Autowired
+    private RapidSuggestService rapidSuggestService;
+
     @PostMapping(value = "/post", consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity getSuggestion(@RequestBody List<Suggestion> suggestions){
 
         dataIngestionService.submitSuggestion(suggestions);
         return new ResponseEntity(HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = "/rapidsuggest", consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity autocomplete(@RequestParam("q") String query,
+        @RequestParam(name = "n", defaultValue = "10") int n,
+    @RequestParam(name = "g", defaultValue = "movies, people") String groups){
+
+
+    Set<Suggestion> results = rapidSuggestService.getSuggestions(n, groups, query);
+        return new ResponseEntity(results, HttpStatus.OK);
+
 
     }
 
